0bfaa424934d986908e8fb71e0f7cd155d1cf05e
Майоров Андрей Артемьевич <a.a.maiorov@Med.Samgmu.host>
2023-03-29 17:20:13 +0400

    change styles

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 import photo from "../../photo.png"
 
-const SidebarEl = styled.div``
+const SidebarEl = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 0 10px;
+`
+
+
 
 export const Sidebar = () => {
     return <SidebarEl>
